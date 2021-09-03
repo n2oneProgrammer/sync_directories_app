8e0b44cdd0bb6a2099d71299489965d67d14906b
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-09-03 23:49:14 +0200

    copying files

--- sync_core.py
+++ sync_core.py
@@ -1,5 +1,6 @@
 import os
 import filecmp
+from shutil import copyfile
 
 
 class SyncCore:
@@ -26,7 +27,18 @@ class SyncCore:
             src2 = os.path.join(self.src_dir2, com)
             self.compare_dirs(src1, src2)
 
+    def copying_file(self, src_file, to_dir1=False):
+        dst_file = os.path.relpath(src_file, self.src_dir2 if to_dir1 else self.src_dir1)
+        dst_file = os.path.join(self.src_dir1 if to_dir1 else self.src_dir2, dst_file)
+        copyfile(src_file, dst_file)
+
     def sync_dir(self):
         self.compare_dirs()
         print(self.left_only)
         print(self.right_only)
+        for src in self.left_only:
+            self.copying_file(src, False)
+
+        for src in self.right_only:
+            self.copying_file(src, True)
+
