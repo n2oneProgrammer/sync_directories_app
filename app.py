231b45d6c286bf19a0976a91cfbd04ae5a47409f
jaanonim <mat8mro@gmail.com>
2021-09-19 16:12:45 +0200

    tray service

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import asyncio
+
+from kivy.config import Config
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 
@@ -6,13 +10,20 @@ from pages.baseclass.main_screen import MainScreen
 from pages.baseclass.settings_screen import SettingsScreen
 from pages.baseclass.sync_screen import SyncScreen
 from utilities.loader import load_kv
+from utilities.path import get_icon_path
 from utilities.screens import ScreensUtilities
 
 
 class SyncDirectories(MDApp):
     def build(self):
+
+        # TODO: fix icon
+        self.icon = get_icon_path()
+        Window.set_icon(self.icon)
+
         self.theme_cls.primary_palette = "Teal"
         self.theme_cls.theme_style = "Dark"
+
         ScreensUtilities.getInstance().sm.add_widget(MainScreen(name="main"))
         ScreensUtilities.getInstance().sm.add_widget(SyncScreen(name="sync"))
         ScreensUtilities.getInstance().sm.add_widget(SettingsScreen(name="settings"))
@@ -23,7 +34,15 @@ class SyncDirectories(MDApp):
     def goTo(self, screen, right):
         ScreensUtilities.getInstance().goTo(screen=screen, right=right)
 
-def run():
+    def on_stop(self):
+        Window.hide()
+
+
+def init():
     load_kv()
     ScreensUtilities.getInstance(sm=ScreenManager())
+
+def run():
+    Window.show()
+    Window.restore()
     SyncDirectories().run()
--- main.py
+++ main.py
@@ -4,31 +4,40 @@ from threading import Thread
 from PIL import Image
 from pystray import Icon, Menu, MenuItem
 
-
-def create_image():
-    image = Image.new("RGB", (64, 64), 0)
-    return image
+from utilities.path import get_icon_path
 
 
-def notify():
-    icon.notify(title="Hello World!", message="xd")
-    time.sleep(3)
-    icon.remove_notification()
+class Tray:
+    def __init__(self):
+        self.initialzed = False
+        self.icon = Icon("SyncDirectories", title="SyncDirectories")
+        self.icon.icon = Image.open(get_icon_path())
+        self.icon.menu = Menu(MenuItem("run", self.start_app))
+        self.icon.run()
 
+    def notify(self, title, message, duration=3):
+        Thread(
+            target=self._notify,
+            name="Notifcation",
+            args=(
+                title,
+                message,
+                duration,
+            ),
+        ).start()
 
-def on_click():
-    Thread(target=notify).start()
+    def _notify(self, title, message, duration):
+        self.icon.notify(title=title, message=message)
+        time.sleep(duration)
+        self.icon.remove_notification()
 
+    def start_app(self):
+        from app import init, run
 
-def run():
-    from app import run
+        if not self.initialzed:
+            init()
+        run()
 
-    run()
-
-
-icon = Icon("test name", title="xd")
-icon.icon = create_image()
-icon.menu = Menu(MenuItem("text", on_click), MenuItem("run", run))
 
 if __name__ == "__main__":
-    icon.run()
+    Tray()
