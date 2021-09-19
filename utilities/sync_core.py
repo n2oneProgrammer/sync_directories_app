dd4463317fb4d50fcedcd989972e9d0c6214f152
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-09-19 12:35:31 +0200

    save conflicts

--- utilities/sync_core.py
+++ utilities/sync_core.py
@@ -1,13 +1,12 @@
 import json
-import json
 import os
 import shutil
-from os.path import basename, exists, isdir, join, relpath
+from os.path import basename, exists, isdir, join, relpath, normpath
 from shutil import copyfile
 
 from deepdiff import DeepDiff
 
-from utilities.errors_type import ConflictsType
+from utilities.conflicts_type import ConflictsType
 from utilities.hash import md5
 
 
@@ -49,21 +48,45 @@ class SyncCore:
 
         return added_items, removed_items, edited_items
 
+    @staticmethod
+    def include_conflict_tab(conflicts, path):
+        for conflict in conflicts:
+            if conflict["left"] == path:
+                return conflict["type"]
+            if conflict["right"] == path:
+                return conflict["type"]
+        return None
+
+    def generate_structure_with_conflicts(self, src_dir, conflicts, old_structure, start_dir=None):
+        result_struct = {}
+        if start_dir is None:
+            start_dir = src_dir
+        struct = [normpath(join(src_dir, f)) for f in os.listdir(src_dir)]
+        for obj in struct:
+            if basename(obj) == self.SYNC_STRUCT_FILE:
+                continue
+            conflicts_type = self.include_conflict_tab(conflicts, obj)
+            src = relpath(obj, start_dir)
+            if conflicts_type is not None:
+                if conflicts_type != ConflictsType.AddAdd:
+                    result_struct[src] = old_structure[src]
+            elif isdir(obj):
+                result_struct[src] = self.generate_structure_with_conflicts(obj, conflicts, old_structure, start_dir)
+            else:
+                result_struct[src] = md5(obj)
+        return result_struct
+
     def sync_dir(self):
         if not exists(join(self.src_dir1, self.SYNC_STRUCT_FILE)):
-            dir_struct = self.generate_structure(self.src_dir1)
             with open(join(self.src_dir1, self.SYNC_STRUCT_FILE), 'w') as outfile:
-                json.dump(dir_struct, outfile)
+                json.dump({}, outfile)
 
         with open(join(self.src_dir1, self.SYNC_STRUCT_FILE), 'r') as infile:
             old = json.load(infile)
-            new = self.generate_structure(self.src_dir1)
-            dir1_diff_add, dir1_diff_del, dir1_diff_edit = SyncCore.compare_dictionary(old, new)
-
-        with open(join(self.src_dir1, self.SYNC_STRUCT_FILE), 'r') as infile:
-            old = json.load(infile)
-            new = self.generate_structure(self.src_dir2)
-            dir2_diff_add, dir2_diff_del, dir2_diff_edit = SyncCore.compare_dictionary(old, new)
+            new1 = self.generate_structure(self.src_dir1)
+            new2 = self.generate_structure(self.src_dir2)
+            dir1_diff_add, dir1_diff_del, dir1_diff_edit = SyncCore.compare_dictionary(old, new1)
+            dir2_diff_add, dir2_diff_del, dir2_diff_edit = SyncCore.compare_dictionary(old, new2)
         print(dir1_diff_add, dir1_diff_del, dir1_diff_edit)
         print(dir2_diff_add, dir2_diff_del, dir2_diff_edit)
         conflicts_tab = []
@@ -77,9 +100,7 @@ class SyncCore:
         conflicts_tab.extend(self.editing_dir_file(dir1_diff_edit, dir2_diff_edit, False))
         conflicts_tab.extend(self.editing_dir_file(dir2_diff_edit, dir1_diff_edit, True))
 
-        print(conflicts_tab)
-
-        dir_struct = self.generate_structure(self.src_dir1)
+        dir_struct = self.generate_structure_with_conflicts(self.src_dir1, conflicts_tab, old)
         with open(join(self.src_dir1, self.SYNC_STRUCT_FILE), 'w') as outfile:
             json.dump(dir_struct, outfile)
 
@@ -87,7 +108,8 @@ class SyncCore:
 
     def generate_added_dir_file(self, diff1, diff2, revers=False):
         result_conflicts = []
-        for d in diff1:
+        copy_diff1 = diff1.copy()
+        for d in copy_diff1:
             diff1.remove(d)
             if revers:
                 src = join(self.src_dir2, d)
@@ -95,25 +117,27 @@ class SyncCore:
             else:
                 src = join(self.src_dir1, d)
                 dst = join(self.src_dir2, d)
+
             if d in diff2:
                 diff2.remove(d)
                 result_conflicts.append({
-                    "left": src,
-                    "right": dst,
+                    "left": normpath(src),
+                    "right": normpath(dst),
                     "type": ConflictsType.AddAdd
                 })
                 print("ERROR I dont know what i should do with " + d)
             else:
-
                 if isdir(src):
                     shutil.copytree(src, dst)
                 else:
                     copyfile(src, dst)
+
         return result_conflicts
 
     def removing_deleted_dir_file(self, remove1, edit2, revers=False):
         result_conflicts = []
-        for r in remove1:
+        copy_remove1 = remove1.copy()
+        for r in copy_remove1:
             remove1.remove(r)
             if revers:
                 src = join(self.src_dir2, r)
@@ -124,8 +148,8 @@ class SyncCore:
             if r in edit2:
                 edit2.remove(r)
                 result_conflicts.append({
-                    "left": src,
-                    "right": target,
+                    "left": normpath(src),
+                    "right": normpath(target),
                     "type": ConflictsType.RemoveEdit
                 })
                 print("ERROR I dont know what i should do with " + r)
@@ -138,7 +162,8 @@ class SyncCore:
 
     def editing_dir_file(self, edit1, edit2, revers=False):
         result_conflicts = []
-        for r in edit1:
+        copy_edit1 = edit1.copy()
+        for r in copy_edit1:
             edit1.remove(r)
             if revers:
                 src = join(self.src_dir2, r)
@@ -149,8 +174,8 @@ class SyncCore:
             if r in edit2:
                 edit2.remove(r)
                 result_conflicts.append({
-                    "left": src,
-                    "right": dst,
+                    "left": normpath(src),
+                    "right": normpath(dst),
                     "type": ConflictsType.EditEdit
                 })
                 print("ERROR I dont know what i should do with " + r)
