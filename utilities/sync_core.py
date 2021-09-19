df1167b0cdb94abbb167db6b1a6df689dcad2733
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-09-19 11:51:14 +0200

    returning conflicts

--- utilities/sync_core.py
+++ utilities/sync_core.py
@@ -7,6 +7,7 @@ from shutil import copyfile
 
 from deepdiff import DeepDiff
 
+from utilities.errors_type import ConflictsType
 from utilities.hash import md5
 
 
@@ -108,72 +109,95 @@ class SyncCore:
             dir2_diff_add, dir2_diff_del, dir2_diff_edit = SyncCore.compare_dictionary(old, new)
         print(dir1_diff_add, dir1_diff_del, dir1_diff_edit)
         print(dir2_diff_add, dir2_diff_del, dir2_diff_edit)
-        self.generate_added_dir_file(dir1_diff_add, dir2_diff_add, False)
-        self.generate_added_dir_file(dir2_diff_add, dir1_diff_add, True)
+        conflicts_tab = []
 
-        self.removing_deleted_dir_file(dir1_diff_del, dir2_diff_edit, False)
-        self.removing_deleted_dir_file(dir2_diff_del, dir1_diff_edit, True)
+        conflicts_tab.extend(self.generate_added_dir_file(dir1_diff_add, dir2_diff_add, False))
+        conflicts_tab.extend(self.generate_added_dir_file(dir2_diff_add, dir1_diff_add, True))
 
-        self.editing_dir_file(dir1_diff_edit, dir2_diff_edit, False)
-        self.editing_dir_file(dir2_diff_edit, dir1_diff_edit, True)
+        conflicts_tab.extend(self.removing_deleted_dir_file(dir1_diff_del, dir2_diff_edit, False))
+        conflicts_tab.extend(self.removing_deleted_dir_file(dir2_diff_del, dir1_diff_edit, True))
+
+        conflicts_tab.extend(self.editing_dir_file(dir1_diff_edit, dir2_diff_edit, False))
+        conflicts_tab.extend(self.editing_dir_file(dir2_diff_edit, dir1_diff_edit, True))
+
+        print(conflicts_tab)
 
         dir_struct = self.generate_structure(self.src_dir1)
         with open(join(self.src_dir1, self.SYNC_STRUCT_FILE), 'w') as outfile:
             json.dump(dir_struct, outfile)
 
-    def generate_added_dir_file(self, diff1, diff2, revers=False):
+        return conflicts_tab
 
+    def generate_added_dir_file(self, diff1, diff2, revers=False):
+        result_conflicts = []
         for d in diff1:
             diff1.remove(d)
+            if revers:
+                src = join(self.src_dir2, d)
+                dst = join(self.src_dir1, d)
+            else:
+                src = join(self.src_dir1, d)
+                dst = join(self.src_dir2, d)
             if d in diff2:
                 diff2.remove(d)
+                result_conflicts.append({
+                    "left": src,
+                    "right": dst,
+                    "type": ConflictsType.AddAdd
+                })
                 print("ERROR I dont know what i should do with " + d)
             else:
-                if revers:
-                    src = join(self.src_dir2, d)
-                    dst = join(self.src_dir1, d)
-                else:
-                    src = join(self.src_dir1, d)
-                    dst = join(self.src_dir2, d)
+
                 if isdir(src):
                     shutil.copytree(src, dst)
                 else:
                     copyfile(src, dst)
+        return result_conflicts
 
     def removing_deleted_dir_file(self, remove1, edit2, revers=False):
-
+        result_conflicts = []
         for r in remove1:
             remove1.remove(r)
+            if revers:
+                src = join(self.src_dir2, r)
+                target = join(self.src_dir1, r)
+            else:
+                src = join(self.src_dir2, r)
+                target = join(self.src_dir2, r)
             if r in edit2:
                 edit2.remove(r)
+                result_conflicts.append({
+                    "left": src,
+                    "right": target,
+                    "type": ConflictsType.RemoveEdit
+                })
                 print("ERROR I dont know what i should do with " + r)
             else:
-
-                if revers:
-                    src = join(self.src_dir2, r)
-                    target = join(self.src_dir1, r)
-                else:
-                    src = join(self.src_dir2, r)
-                    target = join(self.src_dir2, r)
                 if isdir(src):
                     shutil.rmtree(target)
                 else:
                     os.remove(target)
+        return result_conflicts
 
     def editing_dir_file(self, edit1, edit2, revers=False):
-
+        result_conflicts = []
         for r in edit1:
             edit1.remove(r)
+            if revers:
+                src = join(self.src_dir2, r)
+                dst = join(self.src_dir1, r)
+            else:
+                src = join(self.src_dir2, r)
+                dst = join(self.src_dir2, r)
             if r in edit2:
                 edit2.remove(r)
+                result_conflicts.append({
+                    "left": src,
+                    "right": dst,
+                    "type": ConflictsType.EditEdit
+                })
                 print("ERROR I dont know what i should do with " + r)
             else:
-
-                if revers:
-                    src = join(self.src_dir2, r)
-                    dst = join(self.src_dir1, r)
-                else:
-                    src = join(self.src_dir2, r)
-                    dst = join(self.src_dir2, r)
-
                 copyfile(src, dst)
+
+        return result_conflicts
