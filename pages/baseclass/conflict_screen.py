bb54e419c803f413e76093f30e97e0e1e50b7108
jaanonim <mat8mro@gmail.com>
2021-10-09 15:14:02 +0200

    adding conflict validation

--- pages/baseclass/conflict_screen.py
+++ pages/baseclass/conflict_screen.py
@@ -1,4 +1,7 @@
+from kivy.core.window import Window
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.snackbar import Snackbar
 from utilities.conflict_resolver.conflict_resolver_file import ConflictResolverFile
 from utilities.screens import ScreensUtilities
 
@@ -9,7 +12,7 @@ class ConflictScreen(Screen):
         self.conflict = conflict
         self.title.title = self.conflict.path1 + " - " + self.conflict.path2
         self.resolver = ConflictResolverFile(self.conflict, self.sync.sync_core)
-        self.text.text = self.resolver.get_content_merge()
+        self.text.text = self.resolver.get_diff()
 
     def goBack(self):
         ScreensUtilities().goToSync(self.sync, True)
@@ -24,5 +27,14 @@ class ConflictScreen(Screen):
         self.text.text = self.resolver.get_diff()
 
     def save(self):
+        if not self.resolver.is_resolved(self.text.text):
+            Snackbar(
+                text="First you need to resolve it!",
+                snackbar_x="10dp",
+                snackbar_y="10dp",
+                bg_color=(0.8, 0, 0, 1),
+                size_hint_x=(Window.width - (dp(10) * 2)) / Window.width,
+            ).open()
+            return
         self.resolver.resolve(self.text.text)
         ScreensUtilities().goToSync(self.sync, True, True)
