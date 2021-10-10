1937c1b91b7b39964640d07b335a450b4d427937
jaanonim <mat8mro@gmail.com>
2021-10-10 12:43:17 +0200

    using content class

--- pages/baseclass/conflict_screen.py
+++ pages/baseclass/conflict_screen.py
@@ -3,8 +3,11 @@ from ntpath import join
 from kivy.core.window import Window
 from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
+from kivymd.uix.label import MDLabel
 from kivymd.uix.snackbar import Snackbar
-from utilities.conflict_resolver.conflict_resolver_file import ConflictResolverFile
+from utilities.conflict_resolver.conflict_resolver_file import \
+    ConflictResolverFile
 from utilities.screens import ScreensUtilities
 
 
@@ -14,22 +17,55 @@ class ConflictScreen(Screen):
         self.conflict = conflict
         self.title.title = self.conflict.path1 + " - " + self.conflict.path2
         self.resolver = ConflictResolverFile(self.conflict, self.sync.sync_core)
-        self.text.text = self.resolver.get_diff()
+        self.content = self.resolver.get_diff()
+        self.update()
 
     def goBack(self):
         ScreensUtilities().goToSync(self.sync, True)
 
     def file1(self):
-        self.text.text = "".join(self.resolver.get_content_path1())
+        self.content = self.resolver.get_content_path1()
+        self.update()
 
     def file2(self):
-        self.text.text = "".join(self.resolver.get_content_path2())
+        self.content = self.resolver.get_content_path2()
+        self.update()
 
     def compare(self):
-        self.text.text = self.resolver.get_diff()
+        self.content = self.resolver.get_diff()
+        self.update()
+
+    def update(self):
+        self.text.clear_widgets()
+
+        if self.content.is_deleted:
+            self.text.add_widget(
+                MDLabel(
+                    text="This file is deleted",
+                    halign="center",
+                    theme_text_color="Custom",
+                    text_color=(1, 0.1, 0.1, 1),
+                )
+            )
+        elif self.content.is_binary:
+            self.text.add_widget(
+                MDLabel(
+                    text="This file is binary",
+                    halign="center",
+                )
+            )
+        else:
+            self.text.add_widget(
+                TextInput(
+                    background_color=(0.05, 0.1, 0.1, 1),
+                    cursor_color=(0.9, 0.9, 0.9, 1),
+                    foreground_color=(0.9, 0.9, 0.9, 1),
+                    text=self.content.get_content(),
+                )
+            )
 
     def save(self):
-        if not self.resolver.is_resolved(self.text.text):
+        if not self.resolver.is_resolved(self.content):
             Snackbar(
                 text="First you need to resolve it!",
                 snackbar_x="10dp",
@@ -38,5 +74,5 @@ class ConflictScreen(Screen):
                 size_hint_x=(Window.width - (dp(10) * 2)) / Window.width,
             ).open()
             return
-        self.resolver.resolve(self.text.text)
+        self.resolver.resolve(self.content)
         ScreensUtilities().goToSync(self.sync, True, True)
