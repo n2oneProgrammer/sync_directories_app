0f4fd2dc313fe33f77725d24adcdca85122895c3
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-10-21 22:44:44 +0200

    binary, delted file supported

--- utilities/conflict_resolver/conflict_resolver_file.py
+++ utilities/conflict_resolver/conflict_resolver_file.py
@@ -1,6 +1,7 @@
 import difflib
-
+import mime
 from utilities.conflict_resolver.content import Content
+from utilities.conflicts_type import ConflictsType
 
 START_DIFF = ">>>>>>>>>>>>>>>>"
 END_DIFF = "<<<<<<<<<<<<<<<<"
@@ -13,17 +14,24 @@ class ConflictResolverFile:
         self.sync_core = sync_core
 
     def get_content_path1(self):
-        with open(self.conflict.path1, "r") as file:
-            # TODO(any): Check if file is binary
+        if self.conflict.type == ConflictsType.RemoveEdit:
+            is_deleted = True
+            return Content("", True, False)
+        if mime.Types.of(self.conflict.path1)[0].is_binary:
+            return Content("", False, True)
 
+        with open(self.conflict.path1, "r") as file:
             text = file.readlines()
+
         return Content(text, False, False)
 
     def get_content_path2(self):
-        with open(self.conflict.path2, "r") as file:
-            # TODO(any): Check if file is binary
+        if mime.Types.of(self.conflict.path2)[0].is_binary:
+            return Content("", False, True)
 
+        with open(self.conflict.path2, "r") as file:
             text = file.readlines()
+
         return Content(text, False, False)
 
     def resolve(self, new_content):
@@ -45,10 +53,13 @@ class ConflictResolverFile:
         is_new_conflict = ""
         last = ""
         result = ""
-
+        path1 = self.get_content_path1()
+        path2 = self.get_content_path2()
+        if path1.is_binary or path2.is_binary:
+            return Content("", False, True)
         for line in list(
                 difflib.unified_diff(
-                    self.get_content_path1().text, self.get_content_path2().text
+                    path1.text, path2.text
                 )
         )[2:] + [""]:
             if line.startswith("@@"):
@@ -61,7 +72,7 @@ class ConflictResolverFile:
                 elif is_new_conflict == "+" and last == "+":
                     result += BETWEEN_DIFF + "\n"
                 last = "-"
-                result += line.strip() + "\n"
+                result += line.strip()[1:] + "\n"
             elif line.startswith("+"):
                 if is_new_conflict == "":
                     result += START_DIFF + " Right\n"
@@ -69,7 +80,7 @@ class ConflictResolverFile:
                 elif is_new_conflict == "-" and last == "-":
                     result += BETWEEN_DIFF + "\n"
                 last = "+"
-                result += line.strip() + "\n"
+                result += line.strip()[1:] + "\n"
             else:
                 if is_new_conflict == "-":
                     result += END_DIFF + " Right\n"
