9e6b5da51b8b343c77974576648b0197c430d24b
jaanonim <mat8mro@gmail.com>
2021-10-30 18:22:17 +0200

    Revert "Merge pull request #31 from n2oneProgrammer/binary_delete_support"
    This reverts commit d5fcd2c05755089aa6fa5f3d5041662a2058c990, reversing
    changes made to 8e71026e47aa4196f6a53d05eb838e0519ea0a21.

--- utilities/conflict_resolver/conflict_resolver_file.py
+++ utilities/conflict_resolver/conflict_resolver_file.py
@@ -1,7 +1,6 @@
 import difflib
-import mime
+
 from utilities.conflict_resolver.content import Content
-from utilities.conflicts_type import ConflictsType
 
 START_DIFF = ">>>>>>>>>>>>>>>>"
 END_DIFF = "<<<<<<<<<<<<<<<<"
@@ -14,24 +13,17 @@ class ConflictResolverFile:
         self.sync_core = sync_core
 
     def get_content_path1(self):
-        if self.conflict.type == ConflictsType.RemoveEdit:
-            is_deleted = True
-            return Content("", True, False)
-        if mime.Types.of(self.conflict.path1)[0].is_binary:
-            return Content("", False, True)
-
         with open(self.conflict.path1, "r") as file:
-            text = file.readlines()
+            # TODO(any): Check if file is binary
 
+            text = file.readlines()
         return Content(text, False, False)
 
     def get_content_path2(self):
-        if mime.Types.of(self.conflict.path2)[0].is_binary:
-            return Content("", False, True)
-
         with open(self.conflict.path2, "r") as file:
-            text = file.readlines()
+            # TODO(any): Check if file is binary
 
+            text = file.readlines()
         return Content(text, False, False)
 
     def resolve(self, new_content):
@@ -53,13 +45,10 @@ class ConflictResolverFile:
         is_new_conflict = ""
         last = ""
         result = ""
-        path1 = self.get_content_path1()
-        path2 = self.get_content_path2()
-        if path1.is_binary or path2.is_binary:
-            return Content("", False, True)
+
         for line in list(
                 difflib.unified_diff(
-                    path1.text, path2.text
+                    self.get_content_path1().text, self.get_content_path2().text
                 )
         )[2:] + [""]:
             if line.startswith("@@"):
@@ -72,7 +61,7 @@ class ConflictResolverFile:
                 elif is_new_conflict == "+" and last == "+":
                     result += BETWEEN_DIFF + "\n"
                 last = "-"
-                result += line.strip()[1:] + "\n"
+                result += line.strip() + "\n"
             elif line.startswith("+"):
                 if is_new_conflict == "":
                     result += START_DIFF + " Right\n"
@@ -80,7 +69,7 @@ class ConflictResolverFile:
                 elif is_new_conflict == "-" and last == "-":
                     result += BETWEEN_DIFF + "\n"
                 last = "+"
-                result += line.strip()[1:] + "\n"
+                result += line.strip() + "\n"
             else:
                 if is_new_conflict == "-":
                     result += END_DIFF + " Right\n"
