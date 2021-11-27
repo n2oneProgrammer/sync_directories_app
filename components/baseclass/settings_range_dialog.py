ffbd3a1c648151f9bc4cf816a0ec55dcda258c68
jaanonim <mat8mro@gmail.com>
2021-11-27 14:47:03 +0100

    fixing slider

--- components/baseclass/settings_range_dialog.py
+++ components/baseclass/settings_range_dialog.py
@@ -2,6 +2,8 @@ from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class SettingsRangeDialog(MDBoxLayout):
-    def __init__(self, value, **kwargs):
+    def __init__(self, value, min, max, **kwargs):
         super().__init__(**kwargs)
         self.ids.field.value = value
+        self.ids.field.min = min
+        self.ids.field.max = max
